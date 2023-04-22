34a62664a0c139d8e54994eb56f6fc0da54346bd
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-04-21 22:48:55 -0600

    Comunicacion entre dos componentes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["nezuko", "dragon ball"]);
+
+  const onAddCategory = (newCategory) => {
+    console.log(newCategory)
+    setCategories([newCategory, ...categories]);
+  }
+
+  console.log(categories);
+
   return (
     <>
-     <h1>GifExpertApp</h1> 
-    </>
-  )
-}
+      <h1>GifExpertApp</h1>
 
+      {/* input */}
+      <AddCategory 
+        // setCategories={setCategories}
+        onNewCategory={(event) => onAddCategory(event)}
+      />
+
+      {/* listadode gif  */}
+      <ol>
+        {categories.map((caregory) => {
+          return <li key={caregory}>{caregory}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
