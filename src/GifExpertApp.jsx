128f01d24a37ffaf757b284c42b51c956625e407
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-04-21 23:18:04 -0600

    validacion de categorias unicas

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,6 +5,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["nezuko", "dragon ball"]);
 
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     console.log(newCategory)
     setCategories([newCategory, ...categories]);
   }
