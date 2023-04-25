37a13f48658c1e1b54f388bc66183448cbd1d22b
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-04-24 22:10:30 -0600

    Custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
-import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+
+  const {images, isLoading} = useFetchGifs(category);
+
+  console.log(isLoading);
   
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
 
   return (
     <>
