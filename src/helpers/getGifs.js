2c242892ee1879879b3c9ecb1587ee5d7d17273e
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-04-25 11:24:41 -0600

    Se agrega busqueda de 10 elementos

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=TvngPI5lHDL9rlHO01xiLIr4DV2h8cu8&q=${category}&limit=3`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=TvngPI5lHDL9rlHO01xiLIr4DV2h8cu8&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data = [] } = await resp.json();
   
