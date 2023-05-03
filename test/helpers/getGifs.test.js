ec40aae66031b972683bf233c5db0e5188112ff9
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-05-03 10:14:15 -0600

    se agrega pruba en getGifs

---
+++ test/helpers/getGifs.test.js
@@ -0,0 +1,19 @@
+import { getGifs } from "../../src/helpers/getGifs";
+
+describe('Pruebas en getgifs', () => {
+  
+    test('Debe retornar un arreglo de gifs ', async() => {
+      
+        const gifs = await getGifs('tanjiro');
+        // console.log(gifs)
+        expect(gifs.length).toBeGreaterThan(0);
+        expect(gifs[0]).toEqual({
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String)
+        })
+
+    })
+    
+
+})
